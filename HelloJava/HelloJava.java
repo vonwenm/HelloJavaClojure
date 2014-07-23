1ec61503fdd26696e5eaca5c63519b12bd46a29d
Marcelle von Wendland <mvw@hivves.com>
2014-07-23 15:17:12 +0100

    start

--- HelloJava/HelloJava.java
+++ HelloJava/HelloJava.java
@@ -1,3 +1,4 @@
+
 import ThingOne.*;
 
 class HelloJava {
---
+++ HelloJava/compile.bat
@@ -0,0 +1,2 @@
+javac  -cp C:\Users\Marcelle\git\lab\clojure\HiJava\TOne\target\ThingOne-1.0.0-SNAPSHOT-standalone.jar HelloJava.java
+jar cvf classes.jar *.class
